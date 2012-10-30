98bc0442bb5dbd77bf169fd2b0e98b762d7cd0f9
Lolinder <nschulzke@gmail.com>
2012-10-29 21:48:01 -0600

    changed comment

--- subterrain/CommonProxy.java
+++ subterrain/CommonProxy.java
@@ -13,8 +13,6 @@ public class CommonProxy {
 	public static String ITEMS_PNG = "/lolinder/subterrain/items.png";
 	public static String BLOCK_PNG = "/lolinder/subterrain/block.png";
 	
-	// Client stuff
-	public void registerRenderers() {
-		// Nothing here as this is the server side proxy
-	}
+	//To be overridden by ClientProxy
+	public void registerRenderers() {	}
 }
